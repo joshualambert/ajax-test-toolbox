f4df1eb8dbc76706c8218d86bd745a4bbae343f9
Joshua Lambert <joshua.lambert@zettageek.net>
2013-04-24 09:28:56 -0500

    Add commenting to timeout.php

--- timeout.php
+++ timeout.php
@@ -1,8 +1,18 @@
 <?php
 
+/*
+ * timeout.php
+ * Used to test a server timeout.
+ * WARNING: This script will loop in infinity until it is killed by the server. This is not recommended for use in a production environment.
+ */
+
 $me = 'hello';
 while ($me == 'hello') {
 	null; // Do nothing. :) This is to cause a server timeout.
 }
+
+/*
+ * EOF
+ */
 
 ?>
